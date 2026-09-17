2dfa420068e774092af8f8d36ded4ee5df6150c4
Jonathan W. <150624002+marte2050@users.noreply.github.com>
2026-09-17 10:45:03 -0300

    Add __init__.py for ETL module with loader types and default loaders

---
+++ catmat/etl/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from catmat.etl.loaders import LOADER_TYPES, RESOURCES, BaseLoader, default_loaders
+
+__all__ = ["LOADER_TYPES", "RESOURCES", "BaseLoader", "default_loaders"]
